0a5bcfb72918486d972425301b174f2143129dc8
chaimalk <108950943+chaimalk@users.noreply.github.com>
2022-10-03 03:06:07 +0200

    Create knock-at-the-cabin.ads

---
+++ knock-at-the-cabin.ads
@@ -0,0 +1,55 @@
+{
+	"Playstore": {
+
+		"AdsController": {
+
+			"NetworkAds": "admob",
+
+			"BannerAdmob": "ca-app-pub-7448804041969308/3154977182",
+			"InterstitialAdmob": "ca-app-pub-7448804041969308/3713380384",
+
+			"BannerFacebook": "1190494448168418_1190494924835037",
+			"InterstitialFacebook": "1190494448168418_1190495374834992",
+			
+			"unityGameID": "4954605",
+			"UnityPlacement": "Interstitial_Android",
+
+			"StartappAds": "209244357",
+
+			"Cpa": false,
+			"ImageUrl": "https://i.imgur.com/oWEjQGy.gif",
+			"LinkCpa1": "https://cpbldi.com/911232e",
+			"LinkCpa2": "https://google.com/",
+
+			"watchNow": true,
+			"Rating": true
+
+		}
+
+	},
+	"Huaweistore": {
+
+		"AdsController": {
+
+			"NetworkAds": "startapp",
+
+			"BannerAdmob": "ca-app-pub-8082149907872048/3898325980",
+			"InterstitialAdmob": "ca-app-pub-8082149907872048/8781442418",
+
+			"BannerFacebook": "1347348165628536_1347349145628438",
+			"InterstitialFacebook": "1347348165628536_1347353132294706",
+
+			"StartappAds": "211203742",
+
+			"Cpa": false,
+			"ImageUrl": "https://i.imgur.com/oWEjQGy.gif",
+			"LinkCpa1": "https://cpbldi.com/911232e",
+			"LinkCpa2": "https://google.com/",
+
+			"watchNow": false,
+			"Rating": false
+
+		}
+
+	}
+}
